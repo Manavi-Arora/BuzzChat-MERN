6a9f00951ee4d68879e115e3bdaca9f7d9e108d3
Manavi Arora <manaviarora01@gmail.com>
2025-01-07 12:55:38 +0530

    Added Message Routes

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -109,7 +109,7 @@ export const updateProfile = async (req, res) => {
 
     res.status(200).json(updatedUser);
   } catch (error) {
-    console.log("error in update profile:", error);
+    console.log("error in update profile:", error.message);
     res.status(500).json({ message: "Internal server error" });
   }
 };
